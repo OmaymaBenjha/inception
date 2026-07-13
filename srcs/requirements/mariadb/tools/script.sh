ecebedbe8e51dbee5b6f7f3fce65584e97e019d8
OmaymaBenjha <omaimabenjha3@gmail.com>
2026-07-13 05:55:51 -0500

    fixed the wp cnx to mariadb db problem

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
+set -e
 
 mkdir -p /run/mysqld
-
-chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /run/mysqld /var/lib/mysql
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-	mysql_install_db --user=mysql --datadir=/var/lib/mysql
-	
-	cat << EOF > /tmp/db.sql
-CREATE DATABASE IF NOT EXISTS $SQL_DB;
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
+
+mysqld --user=mysql --bootstrap <<EOF
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS \`$SQL_DB\`;
 CREATE USER IF NOT EXISTS '$SQL_USR'@'%' IDENTIFIED BY '$SQL_PWD';
-GRANT ALL PRIVILEGES ON $SQL_DB.* TO '$SQL_USR'@'%';
+GRANT ALL PRIVILEGES ON \`$SQL_DB\`.* TO '$SQL_USR'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PWD';
 FLUSH PRIVILEGES;
 EOF
 
-	/usr/sbin/mysqld --user=mysql --bootstrap < /tmp/db.sql
-
-	rm -f /tmp/db.sql
-fi
-exec /usr/sbin/mysqld --user=mysql
-
+exec mysqld --user=mysql --port=3306 --bind-address=0.0.0.0
